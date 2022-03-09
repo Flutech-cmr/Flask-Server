175ac013c9b3ccb59082e4576703501f334768e7
Anand Mahesh <manand881@gmail.com>
2022-03-09 11:54:20 +0530

    changed ports back

--- Dockerfile
+++ Dockerfile
@@ -44,5 +44,5 @@ WORKDIR /home/Flutech_ERP/Flask-Server
 RUN git pull
 COPY parameters.json .
 
-EXPOSE 80
+EXPOSE 5050
 CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -203,4 +203,4 @@ def not_found(e):
 
 if __name__ == '__main__':
     telegramdebug()
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=5050)
