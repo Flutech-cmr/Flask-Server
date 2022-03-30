d2a274ff04fddf7a541551763378a6aa779a880e
Anand <manand881@gmail.com>
2022-03-30 11:59:21 +0530

    populate workers on attendance page

--- static/js/attendance.js
+++ static/js/attendance.js
@@ -1,3 +1,16 @@
+var PopulateWorkeronPage = function (workername, contractor, labourtype, key) {
+    const template = document.getElementsByTagName("template")[0]
+    const allworkers = document.getElementById("all_workers")
+    let clone = template.content.cloneNode(true)
+    // set clone id
+    clone.firstChild.id = "worker " + key
+    allworkers.appendChild(clone)
+    const intkey = parseInt(key)
+    document.getElementsByClassName("workername")[intkey].innerHTML = workername
+    document.getElementsByClassName("contractorname")[intkey].innerHTML = contractor
+    document.getElementsByClassName("LabourType")[intkey].innerHTML = labourtype
+}
+
 var GetAllEmployees = function () {
     const xhr = new XMLHttpRequest()
     xhr.open("GET", "/getallemployees", true)
@@ -9,9 +22,11 @@ var GetAllEmployees = function () {
             const ResponseTextAllWorkers = JSON.parse(xhr.responseText)
             for (const [key, value] of Object.entries(ResponseTextAllWorkers)) {
                 console.log(key, value)
+                const workername = value["Workername"]
+                const contractor = value["ContractorID"]
+                const labourtype = value["LabourType"]
+                PopulateWorkeronPage(workername, contractor, labourtype, key)
             }
         }
     }
 }
-
-console.log("Hi Frands")
