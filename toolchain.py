24295ad090918cea95f8ad9f4543c145df67d4ff
Anand Mahesh <manand881@gmail.com>
2022-04-12 11:15:46 +0530

    pushing a fee changes to be metrged with work in progress branch

--- toolchain.py
+++ toolchain.py
@@ -3,38 +3,53 @@ import sys
 import time
 import requests
 
+commit_message = None
+payload = {"to_terminal": "git pull"}
+server_url = "https://comfytronics.in/"
+
 try:
     commit_message = sys.argv[1]
 except:
     print("[INFO] This script requires a commit message as an argument. No commit message was provided.\n")
     sys.exit(1)
 
+
 def iterateversion():
     print("[INFO] Iterating Version")
-    f=open("version","r")
-    lines=f.readlines()
-    lines=float(lines[0])
-    lines+=0.1
-    lines=round(lines,3)
+    f = open("version", "r")
+    lines = f.readlines()
+    lines = float(lines[0])
+    lines += 0.1
+    lines = round(lines, 3)
     f.close()
-    f=open("version","r+")
+    f = open("version", "r+")
     f.write(str(lines))
     f.close()
 
-payload = {"to_terminal": "git pull"}
-server_url="https://comfytronics.in/"
+
+# generate requirements.txt
 print("[INFO] generating requirements\n")
 os.system("pipreqs  --force")
+
+# iterate the version in the version file by 0.1
 iterateversion()
+
+# add all files commit them with the message recieved from the system arguements and push
 print("[INFO] performing git push\n")
 os.system(f'git add --all')
 os.system(f'git commit -m "{commit_message}"')
 os.system(f'git push')
+
+# sleep for 10 seconds to allow github to refresh
 print("[INFO] sleeping for 10 seconds\n")
 time.sleep(10)
+
+# check if the server is running
 print("[INFO] Checking If Server is running\n")
-r=requests.get(server_url+"/up")
+r = requests.get(server_url+"/up")
 if r.status_code == 200:
+
+    # instruct server to perform a git pull if running
     print("[INFO] performing git pull on server\n")
     r = requests.post(server_url+'/git', json=payload)
     if r.status_code == 200:
