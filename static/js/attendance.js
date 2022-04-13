e2672ca925e7c1146195364d2fea36fd52dc59bc
anand mahesh <anandmahesh@anands-MacBook-Pro.local>
2022-04-13 16:17:19 +0530

    fixed a few bugs

--- static/js/attendance.js
+++ static/js/attendance.js
@@ -1,3 +1,16 @@
+var EnableDisableButtons = function (id) {
+    const intimebuttons = document.getElementsByClassName("intime")[id]
+    const outtimebuttons = document.getElementsByClassName("outtime")[id]
+    if (intimebuttons.innerHTML.includes("In-Time")) {
+        intimebuttons.disabled = false
+        outtimebuttons.disabled = true
+    }
+    else if (outtimebuttons.innerHTML.includes("Out-Time") && !intimebuttons.innerHTML.includes("In-Time")) {
+        intimebuttons.disabled = true
+        outtimebuttons.disabled = false
+    }
+}
+
 var postattendance = function (payload) {
     const xhr = new XMLHttpRequest()
     xhr.open("POST", "/workerattendance", true)
@@ -15,7 +28,6 @@ var postattendance = function (payload) {
                         buttonin.disabled = true
                         buttonin.style.backgroundColor = "grey"
                         document.getElementsByClassName("outtime")[payload["id"]].disabled = false
-                        document.getElementsByClassName("outtime")[payload["id"]].style.backgroundColor = rgb(219, 39, 119);
                     }
                     else if (response["type"] == "outtime") {
                         const buttonout = document.getElementsByClassName("outtime")[payload["id"]]
@@ -24,6 +36,7 @@ var postattendance = function (payload) {
                         buttonout.disabled = true
                     }
                 }
+                EnableDisableButtons(payload["id"])
             }
         }
     }
@@ -47,6 +60,7 @@ var postattendance = function (payload) {
                         buttonout.disabled = true
                     }
                 }
+                EnableDisableButtons(payload["id"])
             }
         }
     }
@@ -76,7 +90,7 @@ var MarkOutTime = function (id, workername, contractor, labourtype) {
     postattendance(payload)
 }
 
-var CheckPreviousAttendance = function (id, workername, contractor, labourtype) {
+var CheckPreviousAttendance = async function (id, workername, contractor, labourtype) {
     payload = {
         "Workername": workername,
         "ContractorID": contractor,
@@ -88,14 +102,9 @@ var CheckPreviousAttendance = function (id, workername, contractor, labourtype) 
     postattendance(payload)
     payload["type"] = "outtime"
     postattendance(payload)
-    const buttonout = document.getElementsByClassName("outtime")[payload["id"]]
-    console.log(buttonout.innerHTML)
-    const buttonin = document.getElementsByClassName("intime")[payload["id"]]
-    if (buttonin.innerHTML.includes("Mark In-Time")) {
-        buttonout.disabled = true
-    }
 }
 
+// this function is responsible to populate all the workers on the page
 var PopulateWorkeronPage = function (workername, contractor, labourtype, key) {
     const allworkers = document.getElementById("all_workers")
     const template = document.getElementsByTagName("template")[0]
@@ -129,11 +138,13 @@ var GetAllEmployees = function () {
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             const ResponseTextAllWorkers = JSON.parse(xhr.responseText)
+            let maxkey = 0
             for (const [key, value] of Object.entries(ResponseTextAllWorkers)) {
                 const workername = value["Workername"]
                 const contractor = value["ContractorID"]
                 const labourtype = value["LabourType"]
                 PopulateWorkeronPage(workername, contractor, labourtype, key)
+                maxkey += 1
             }
         }
     }
